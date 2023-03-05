f5885c74b114f6276fc01a73ba91634f62a1cd04
okawahiroto <okawa.hiroto@gmail.com>
2023-03-05 09:46:40 +0900

    ブラウザからカメラを起動してブラウザ上に表示

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("app.js loaded");
+      // getUserMediaでカメラから映像を取得する
+      navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function(stream) {
+        // Video要素にカメラからの映像ストリームをセットする
+        var video = document.getElementById('camera-stream');
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function(err) {
+        console.log('getUserMediaでカメラの取得に失敗しました：', err);
+      });
