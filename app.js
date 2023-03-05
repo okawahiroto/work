9939808ac53eeed86ba11a85fc3e49ee952a5d4b
okawahiroto <okawa.hiroto@gmail.com>
2023-03-05 10:25:43 +0900

    カメラの設定

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 console.log("app.js loaded");
 // getUserMediaでカメラから映像を取得する
-navigator.mediaDevices.getUserMedia({video: true})
-
+navigator.mediaDevices.getUserMedia(
+    //マイクはオフ, カメラの設定   背面カメラを希望する 640×480を希望する
+    {"audio":false,"video":{"facingMode":"environment","width":{"ideal":640},"height":{"ideal":480}}}
+)
 .then(function(stream) {
   // Video要素にカメラからの映像ストリームをセットする
   var video = document.getElementById('camera-stream');
