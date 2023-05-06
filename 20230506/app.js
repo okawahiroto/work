7249de03704ede9fc99b86fd1b1b8a76ae564dd2
okawahiroto <okawa.hiroto@gmail.com>
2023-05-07 07:59:50 +0900

    複数の調から選択(ラジオボタン・グループ)、調に合わせて画像を変更

--- 20230506/app.js
+++ 20230506/app.js
@@ -1,18 +1,19 @@
 console.log('app.js loaded');
 
-const element = document.querySelector('#radioGroup');
+// const element = document.querySelector('#radioGroup');
+const element = document.querySelector('#keyGroupSharp');
 const result = document.querySelector('#result');
 const img = document.querySelector('#img');
 
 console.log(element);
-console.log(element.radio.value);
+console.log(element.key.value);
 
 element.addEventListener('change', handleChange);
 
 function handleChange(event) {
-  const radioValue = element.radio.value;
-  console.log(radioValue);
-  result.textContent = radioValue;
-  img.src = 'img/' + radioValue + '.png'
+  const keyValue = element.key.value;
+  console.log(keyValue);
+  result.textContent = keyValue;
+  img.src = 'img/' + keyValue + '.png'
 }
 
